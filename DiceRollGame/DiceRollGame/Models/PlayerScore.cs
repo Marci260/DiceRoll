dcf47adcffaecceb97622278fbf6b12b6156c82d
Kánya Marcell <marcikanya@gmail.com>
2026-05-04 09:15:07 +0200

    init base playerscore model

---
+++ DiceRollGame/DiceRollGame/Models/PlayerScore.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+namespace DiceRollGame.Models
+{
+    public class PlayerScore
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string PlayerName { get; set; }
+        public int CurrentScore { get; set; }
+        public string GameName { get; set; }
+    }
+}

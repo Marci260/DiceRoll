f559d2925a6cf237630a1472f648dcdff6464145
Kánya Marcell <marcikanya@gmail.com>
2026-05-04 09:46:51 +0200

    playerdetails view model done

---
+++ DiceRollGame/DiceRollGame/ViewModels/PlayerDetailViewModel.cs
@@ -0,0 +1,40 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DiceRollGame.Models;
+using DiceRollGame.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRollGame.ViewModels
+{
+    public partial class PlayerDetailViewModel : ObservableObject
+    {
+        private readonly DatabaseService _databaseService;
+
+        [ObservableProperty]
+        private PlayerScore _player;
+
+        public PlayerDetailViewModel(DatabaseService databaseService)
+        {
+            _databaseService = databaseService;
+            Player = new PlayerScore();
+        }
+
+        [RelayCommand]
+        public async Task SaveAsync()
+        {
+            try
+            {
+                await _databaseService.SavePlayerAsync(Player);
+                await Shell.Current.GoToAsync("..");
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Database Error", ex.Message, "OK");
+            }
+        }
+    }
+}

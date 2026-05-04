6868fe6ef055cf3cbf49dcc0a82382e51e14d1c0
Kánya Marcell <marcikanya@gmail.com>
2026-05-04 09:52:36 +0200

    dicepage.xml.cs and small fixes

--- DiceRollGame/DiceRollGame/Views/DicePage.xaml.cs
+++ DiceRollGame/DiceRollGame/Views/DicePage.xaml.cs
@@ -1,9 +1,27 @@
+using DiceRollGame.ViewModels;
+
 namespace DiceRollGame.Views;
 
 public partial class DicePage : ContentPage
 {
-	public DicePage()
-	{
-		InitializeComponent();
-	}
+    private readonly DiceViewModel _viewModel;
+
+    public DicePage(DiceViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+        _viewModel = viewModel;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        _viewModel.StartShakeDetectionCommand.Execute(null);
+    }
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+        _viewModel.StopShakeDetectionCommand.Execute(null);
+    }
 }
